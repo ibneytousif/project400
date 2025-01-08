a534de78f3eb52d16eaf2d9e7452ce22095ac2e6
Ibney Tousif Rupom <itbangladesh.47@gmail.com>
2025-01-08 16:12:51 +0600

    Update oldscript.js

--- oldscript.js
+++ oldscript.js
@@ -12,10 +12,10 @@ function initMap() {
     const icons = {
         restaurant: {
             url: "cutlery.png", // Icon for restaurants
-            scaledSize: new google.maps.Size(30, 30),
+            scaledSize: new google.maps.Size(20, 20),
         },
         shoppingMall: {
-            url: "placeholder.png", // Icon for shopping malls
+            url: "placeholder.png", // Icon for shopping malls 
             scaledSize: new google.maps.Size(30, 30),
         },
         freespace: {
@@ -57,10 +57,59 @@ function initMap() {
         { position: { lat: 23.780627510369293, lng: 90.4172576043047 }, title: "Cielo Rooftop - Gulshan", category: "restaurant" },
         { position: { lat: 23.78058639784169, lng: 90.41734611720241 }, title: "Fakhruddin Biriyani & Restaurant", category: "restaurant" },
         { position: { lat: 23.780654090674524, lng: 90.41763671165883 }, title: "Tasty Treat (Needs Confirmation)", category: "restaurant" },
+        // Category: Restaurants
+        { position: { lat: 23.788640582379717, lng: 90.42058596966801 }, title: "IZUMI Japanese Kitchen", category: "restaurant" },
+        { position: { lat: 23.780759520101675, lng: 90.41842097589713 }, title: "The Kaffeehaus Gulshan", category: "restaurant" },
+        { position: { lat: 23.781846005380995, lng: 90.41760491708095 }, title: "Cafe Mango", category: "restaurant" },
+        { position: { lat: 23.78205952149916, lng: 90.4170677205276 }, title: "tastebud (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.7819496847338, lng: 90.41691215240273 }, title: "BREAD & Beyond - Gulshan S/A (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.781815303313646, lng: 90.41694836222491 }, title: "Sweets of Bengal (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.781818891390916, lng: 90.41736727246213 }, title: "Yum Cha District", category: "restaurant" },
+        { position: { lat: 23.784491902672034, lng: 90.41853122413511 }, title: "Tulip Oasis Restaurant And Banquet Hall", category: "restaurant" },
+        { position: { lat: 23.78501930839126, lng: 90.41955017416889 }, title: "Koreana Restaurant", category: "restaurant" },
+        { position: { lat: 23.785545062093284, lng: 90.41870181062328 }, title: "Pizza Roma", category: "restaurant" },
+        { position: { lat: 23.78565980458892, lng: 90.41862872042668 }, title: "Bene Bistro", category: "restaurant" },
+        { position: { lat: 23.788333594551247, lng: 90.41909382339065 }, title: "Da one Cafe", category: "restaurant" },
+        { position: { lat: 23.788081996130966, lng: 90.41841607799398 }, title: "The Coffee Bean & Tea Leaf", category: "restaurant" },
+        { position: { lat: 23.782649551153156, lng: 90.41690580547208 }, title: "Dhaka New Hall (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.783781043405792, lng: 90.41710663587203 }, title: "Pizza Inn Gulshan 1", category: "restaurant" },
+        { position: { lat: 23.783970114080933, lng: 90.41725513726907 }, title: "Pizza Italiana", category: "restaurant" },
+        { position: { lat: 23.78496729165155, lng: 90.41718594494982 }, title: "Raater Kabab 3 (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.785741687750374, lng: 90.41732905250252 }, title: "Aandaaz", category: "restaurant" },
+        { position: { lat: 23.785750032232222, lng: 90.4173172629156 }, title: "Happiness Cafe", category: "restaurant" },
+        { position: { lat: 23.78757706773768, lng: 90.41659053489262 }, title: "Talent Chef (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.786823774715835, lng: 90.41633376298445 }, title: "Bismillah Kacchi House (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.782347771920332, lng: 90.41584817955018 }, title: "Happy tea", category: "restaurant" },
+        { position: { lat: 23.782360837869085, lng: 90.4153083135403 }, title: "湘菜馆", category: "restaurant" },
+        { position: { lat: 23.786067592543866, lng: 90.416754683833 }, title: "Al-Amar Lebanese Cuisine", category: "restaurant" },
+        { position: { lat: 23.786117293650626, lng: 90.41676541266803 }, title: "Bella Italia", category: "restaurant" },
+        { position: { lat: 23.783607086316763, lng: 90.41617590934383 }, title: "Tasty Treat Gulshan-2 (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.783551248189422, lng: 90.41617792100062 }, title: "Fry Bucket (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.78349234200833, lng: 90.41644815355815 }, title: "Hongbao", category: "restaurant" },
+        { position: { lat: 23.783403369079608, lng: 90.41623827070035 }, title: "Crimson Cup Coffee", category: "restaurant" },
+        { position: { lat: 23.783266696720663, lng: 90.41624674115985 }, title: "Second Cup Coffee", category: "restaurant" },
+        { position: { lat: 23.783170360345473, lng: 90.41626350496686 }, title: "Bosphorus", category: "restaurant" },
+        { position: { lat: 23.782165525333102, lng: 90.41649588132934 }, title: "Taqueria Lola", category: "restaurant" },
+        { position: { lat: 23.781988804818507, lng: 90.41632019663723 }, title: "NORTH END coffee roasters LK Tower 2 (Gender Neutral Toilets)", category: "restaurant" },
+        { position: { lat: 23.781170454777378, lng: 90.41642344872291 }, title: "Tehari Ghor (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.78113609221191, lng: 90.41644960026115 }, title: "Bismillah Hanif Biryani and Restaurant (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.780961824778174, lng: 90.41621691862615 }, title: "Break Time (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.780666568193013, lng: 90.41576615793973 }, title: "Nawab Chatga", category: "restaurant" },
+        { position: { lat: 23.780445664974927, lng: 90.41579432113474 }, title: "Mollika Snacks", category: "restaurant" },
+        { position: { lat: 23.780582657159982, lng: 90.41598702147475 }, title: "Gulshan Darbar Restaurant (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.78095374319618, lng: 90.41553669646626 }, title: "CocoJoy, Banani (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.781443694439982, lng: 90.41577650065369 }, title: "C House Cafe (Needs Confirmation)", category: "restaurant" },
+        { position: { lat: 23.781462102914535, lng: 90.41590591724031 }, title: "Khalifa’s Restaurant", category: "restaurant" },
+        { position: { lat: 23.78191801864455, lng: 90.41605142707998 }, title: "A&W Restaurants Bangladesh", category: "restaurant" },
+        { position: { lat: 23.78193704066546, lng: 90.41601588781008 }, title: "Khao San", category: "restaurant" },
+        { position: { lat: 23.782024787371103, lng: 90.41605880315487 }, title: "Smith’s Caffe Regalo", category: "restaurant" },
+    
 
         // Category: Shopping Malls
         { position: { lat: 23.772896076537897, lng: 90.41620338526414 }, title: "Police Plaza Concord Restaurant", category: "shoppingMall" },
         { position: { lat: 23.776930591065636, lng: 90.41601512104735 }, title: "Unimart Gulshan 1", category: "shoppingMall" },
+        { position: { lat: 23.780604439786103, lng: 90.41628072989106 }, title: "Shopping complex Mosque", category: "shoppingMall" },
+        
         // freeespaceee
         { position: { lat: 23.773696394635916, lng: 90.4152200478557 }, title: "Shahid Dr Fazle Rabbi Park", category: "freespace" },
         // Category: Masjid
